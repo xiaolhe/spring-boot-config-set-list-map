033c605d46faec7291378aaba11c1ed4e378cc4c
宝宝很腻害 <39144603+xiaolhe@users.noreply.github.com>
2021-05-18 14:31:47 +0800

    Update MapParst.java

--- src/main/java/com/xialh/springbootconfig/utils/MapParst.java
+++ src/main/java/com/xialh/springbootconfig/utils/MapParst.java
@@ -6,7 +6,7 @@ import com.alibaba.fastjson.TypeReference;
 import java.util.Map;
 
 /**
- * Created by  肖橙橙
+ * Created by  xiaolhe
  * CreateTime 2020-07-30  15:12
  *  此解析方法为： 解析配置里面的map集合中的key value（前提是,使用了el表达式去配置获取值）
  */
